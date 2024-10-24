24b59853be56435eb87f0caf2d3dcef765865e77
Dymah <160054538+Dymah15@users.noreply.github.com>
2024-10-24 04:14:55 -1000

    Création projet et MainActivity.java

---
+++ app/src/main/java/pf/licence/pm_tp_carnetadresse/MainActivity.java
@@ -0,0 +1,14 @@
+package pf.licence.pm_tp_carnetadresse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
